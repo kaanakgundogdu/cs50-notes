c2dc388340e8581ee5295bdbad5390f3c582cb12
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2023-03-04 17:57:02 +0300

    copy.c modified.

--- 04-Memory/copy.c
+++ 04-Memory/copy.c
@@ -9,15 +9,26 @@ typedef char* string;
 int main(void)
 {
     string s= get_string("s: ");
-
+    if (s==NULL)
+    {
+        return 1;
+    }
+    
     //changes t and s 
     //string t= s;
     string t= malloc(strlen(s)+1);
+    
+    if (t==NULL)
+    {
+        return 1;
+    }
 
     for (size_t i = 0 , len=strlen(s)+1; i < len; i++)
     {
         t[i]=s[i];
     }
+    //or just use
+    //strcpy(t,s);
     
     if(strlen(t)>0)
     {
@@ -27,5 +38,7 @@ int main(void)
     printf("s: %s\n",s);
     printf("t: %s\n",t);
 
+    free(t); 
 
+    return 0;
 }
